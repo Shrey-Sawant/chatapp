69cdbf9dde336cc21ad38cc116de91f95330e1e0
Shrey <163460041+Shrey-Sawant@users.noreply.github.com>
2025-03-25 10:53:14 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,7 +37,10 @@ io.on("connection", (socket) => {
     });
 })
 
-app.use(cors({ origin: ["*", "http://localhost:5173","https://chatapp-kohl-kappa.vercel.app/"], credentials: true }));
+app.use(cors({ origin:[
+  "http://localhost:5173",
+  "https://chatapp-kohl-kappa.vercel.app" // No trailing slash
+], credentials: true }));
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
